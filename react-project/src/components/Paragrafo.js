490a94f1215380103a8aab57c70daa63fbca0ee8
MattheusSantana <matheusess@hotmail.com>
2022-10-27 18:22:13 -0400

    Criando estilos css específico para módulos

--- react-project/src/components/Paragrafo.js
+++ react-project/src/components/Paragrafo.js
@@ -1,6 +1,8 @@
+import styles from "./Paragrafo.module.css";
+
 function Paragrafo(props){
     
-    return <p>{props.text}</p>
+    return <p className={ styles.paragrafoContent}>{props.text}</p>
     
 }
 
