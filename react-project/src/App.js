2968778530f3b43df517d9d4cefdfb9ddc1d9c38
MattheusSantana <matheusess@hotmail.com>
2022-10-28 11:20:02 -0400

    child component calling event in parent component

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -2,11 +2,9 @@ import HelloWorld from './components/HelloWorld';
 import './App.css';
 import AlertButton from './components/AlertButton';
 import Form from './components/Form';
-// import List from './components/List';
 
 function App() {
   const backgroundUrl = "https://media2.giphy.com/media/BYoRqTmcgzHcL9TCy1/giphy.gif?cid=ecf05e47ee9efad348581187448b3e9a3094db953ea3783a&rid=giphy.gif&ct=g";
-  
  
   return (
     <div className="App">
--- react-project/src/components/Form.js
+++ react-project/src/components/Form.js
@@ -1,18 +1,19 @@
 import {useState} from "react";
-
+import Button from "./Button";
 
 function Form(){
-    function signUp(e){
-        e.preventDefault()
-        console.log("submit event");
+  
+    function eventFatherComponent(){
+        console.log('Father component event called!');
     }
+
     const [name, setName] = useState("Visitor");
 
     return (
         <div>
             <h1>WELCOME {name}!</h1>
 
-            <form onSubmit={signUp}>
+          
             <h3>Subscribe:</h3>
                 <div>
                     <input type="text" name="name" onChange={(e) => setName(e.target.value)} placeholder="Name" />
@@ -21,10 +22,8 @@ function Form(){
                     <input type="email" name="email" placeholder="Email"/>
                 </div>
 
-                <div>
-                    <input type="submit" name="subscribe" value="subscribe"/>
-                </div>
-            </form>
+                <Button event={eventFatherComponent} text="Subscribe"/>
+            
         </div>
     )
 }
