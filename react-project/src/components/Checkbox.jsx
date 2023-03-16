037dd1f7558dd2a389ddf868c7a63f6ea9a3bb63
MattheusSantana <matheusess@hotmail.com>
2023-03-16 15:24:41 -0400

    useReducer hook with checkbox toggle

--- react-project/src/components/Checkbox.jsx
+++ react-project/src/components/Checkbox.jsx
@@ -1,15 +1,20 @@
-import React, {useEffect, useState} from 'react'
+import React, {useEffect, useReducer, useState} from 'react'
 
 const Checkbox = () => {
     const [checked, setChecked] = useState(false);
-    
+    const [check, toggle] = useReducer(check => !check, false);
+
+
     useEffect(() => {
-        console.log(checked ? "checked" : "not checked" );
+        console.log(checked ? "checked is checked" : "checked is not checked" );
+        console.log(check ? "check is checked" : "check is not checked" );
+
     });
     
     return (
     <>
     <input type="checkbox" value={checked} onChange={() => setChecked(checked => !checked)} />
+    <input type="checkbox" value={check} onChange={toggle} />
     {checked ? "checked" : "not checked"}
     </>
   );
